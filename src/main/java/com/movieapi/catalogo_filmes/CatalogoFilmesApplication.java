b8a9a85bfde4045bae78d0a11d28f3694f1a99bc
joaopl <jvplucas@hotmail.com>
2024-12-30 18:32:01 -0300

    init

---
+++ src/main/java/com/movieapi/catalogo_filmes/CatalogoFilmesApplication.java
@@ -0,0 +1,13 @@
+package com.movieapi.catalogo_filmes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogoFilmesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogoFilmesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/movieapi/catalogo_filmes/CatalogoFilmesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movieapi.catalogo_filmes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogoFilmesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
